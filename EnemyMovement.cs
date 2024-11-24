3e85c2a2f60e0861a42b60924c44247c1ac4eeee
Kif87 <ahmadakif005@gmail.com>
2024-11-24 10:31:26 +0800

    Update and rename EnemeyMovement.cs to EnemyMovement.cs

--- EnemyMovement.cs
+++ EnemyMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemeyMovement : MonoBehaviour
+public class EnemyMovement : MonoBehaviour
 {
     [SerializeField]
     private float _speed;
